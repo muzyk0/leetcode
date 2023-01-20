d5a3bb00f9956cc6b04543e7644a61df79b52c2e
muzyk0 <ru9art@gmail.com>
2023-01-20 15:35:37 +0300

    remove unused deps, change npm to pnpm

--- src/easy/9.PalindromeNumber.spec.ts
+++ src/easy/9.PalindromeNumber.spec.ts
@@ -1,20 +1,25 @@
-import { twoSum } from './1.TwoSum';
-import {isPalindrome} from "./9.PalindromeNumber";
+import {isPalindrome, isPalindromeWithoutConvert} from "./9.PalindromeNumber";
 
 describe('9. Palindrome Number', function () {
     it('should return true if input data x = 121', function () {
         const res = isPalindrome(121);
+        const res2 = isPalindromeWithoutConvert(121);
 
         expect(res).toBeTruthy();
+        expect(res2).toBeTruthy();
     });
     it('should return false if input data x = -121', function () {
         const res = isPalindrome(-121);
+        const res2 = isPalindromeWithoutConvert(-121);
 
         expect(res).toBeFalsy()
+        expect(res2).toBeFalsy()
     });
     it('should return false if input data ', function () {
         const res = isPalindrome(10);
+        const res2 = isPalindromeWithoutConvert(10);
 
         expect(res).toBeFalsy();
+        expect(res2).toBeFalsy();
     });
 });
