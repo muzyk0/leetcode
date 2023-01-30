79a198bcb02aa88b680f923ca4e5b83c6c2e80e6
muzyk0 <ru9art@gmail.com>
2023-01-30 11:33:03 +0300

    21. Merge Two Sorted Lists

---
+++ src/easy/21.MergeTwoSortedLists/21.MergeTwoSortedLists.spec.ts
@@ -0,0 +1,90 @@
+import {
+  createLinkedList,
+  ListNode,
+  mergeTwoListsRecursive,
+  mergeTwoListsWhile,
+} from "./21.MergeTwoSortedLists";
+
+describe("21. Merge Two Sorted Lists", function () {
+  it("linedList create3d currently", function () {
+    const res = new ListNode(10, { val: 15, next: null });
+
+    const reqEqual = {
+      val: 10,
+      next: {
+        val: 15,
+        next: null,
+      },
+    };
+    expect(res.val).toBe(reqEqual.val);
+    expect(res.next?.val).toBe(reqEqual.next.val);
+    expect(res.next?.next).toBeNull();
+  });
+
+  it("createLinkedList work currently", function () {
+    const res = createLinkedList([1, 1, 2, 3, 4, 4]);
+    const res2 = createLinkedList([0]);
+
+    expect(res).toStrictEqual({
+      val: 1,
+      next: {
+        val: 1,
+        next: {
+          val: 2,
+          next: {
+            val: 3,
+            next: {
+              val: 4,
+              next: {
+                val: 4,
+                next: null,
+              },
+            },
+          },
+        },
+      },
+    });
+    expect(res2).toStrictEqual({
+      next: null,
+      val: 0,
+    });
+  });
+
+  it("should return [1,1,2,3,4,4] if input data list1 = [1,2,4], list2 = [1,3,4]", function () {
+    const res = mergeTwoListsRecursive(
+      createLinkedList([1, 2, 4]),
+      createLinkedList([1, 3, 4])
+    );
+    const res2 = mergeTwoListsWhile(
+      createLinkedList([1, 2, 4]),
+      createLinkedList([1, 3, 4])
+    );
+
+    expect(res).toStrictEqual(createLinkedList([1, 1, 2, 3, 4, 4]));
+    expect(res2).toStrictEqual(createLinkedList([1, 1, 2, 3, 4, 4]));
+  });
+  it("should return [] if input data list1 = [], list2 = []", function () {
+    const res = mergeTwoListsRecursive(
+      createLinkedList([]),
+      createLinkedList([])
+    );
+    const res2 = mergeTwoListsWhile(createLinkedList([]), createLinkedList([]));
+
+    expect(res).toStrictEqual(createLinkedList([]));
+    expect(res2).toStrictEqual(createLinkedList([]));
+  });
+
+  it("should return [0] if input data list1 = [], list2 = [0]", function () {
+    const res = mergeTwoListsRecursive(
+      createLinkedList([]),
+      createLinkedList([0])
+    );
+    const res2 = mergeTwoListsWhile(
+      createLinkedList([]),
+      createLinkedList([0])
+    );
+
+    expect(res).toStrictEqual(createLinkedList([0]));
+    expect(res2).toStrictEqual(createLinkedList([0]));
+  });
+});
---
+++ src/easy/21.MergeTwoSortedLists/21.MergeTwoSortedLists.ts
@@ -0,0 +1,109 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+export class ListNode {
+  val: number;
+  next: ListNode | null;
+
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+/**
+ * 21. Merge Two Sorted Lists
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ *
+ * You are given the heads of two sorted linked lists list1 and list2.
+ *
+ * Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+ *
+ * Return the head of the merged linked list.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: list1 = [1,2,4], list2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ * Example 2:
+ *
+ * Input: list1 = [], list2 = []
+ * Output: []
+ * Example 3:
+ *
+ * Input: list1 = [], list2 = [0]
+ * Output: [0]
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in both lists is in the range [0, 50].
+ * -100 <= Node.val <= 100
+ * Both list1 and list2 are sorted in non-decreasing order.
+ * @param list
+ */
+export const createLinkedList = (list: number[]): ListNode | null => {
+  const linkedList = new ListNode();
+
+  let node = linkedList;
+
+  for (let i = 0; i < list.length; i++) {
+    node.next = {
+      val: list[i],
+      next: null,
+    };
+    node = node.next;
+  }
+
+  return linkedList.next;
+};
+
+export function mergeTwoListsRecursive(
+  list1: ListNode | null,
+  list2: ListNode | null
+): ListNode | null {
+  if (!list1 || !list2) {
+    return list1 ? list1 : list2;
+  }
+
+  if (list1.val < list2.val) {
+    list1.next = mergeTwoListsRecursive(list1.next, list2);
+    return list1;
+  } else {
+    list2.next = mergeTwoListsRecursive(list1, list2.next);
+    return list2;
+  }
+}
+
+export function mergeTwoListsWhile(
+  list1: ListNode | null,
+  list2: ListNode | null
+) {
+  const head = new ListNode();
+  let node = head;
+  while (list1 && list2) {
+    if (list1.val > list2.val) {
+      node.next = list2;
+      list2 = list2.next;
+    } else {
+      node.next = list1;
+      list1 = list1.next;
+    }
+    node = node.next;
+  }
+  node.next = list1 || list2;
+  return head.next;
+}
+
+console.log(JSON.stringify(createLinkedList([1, 2, 3, 4])));
